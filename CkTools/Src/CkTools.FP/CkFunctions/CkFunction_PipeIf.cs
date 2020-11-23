f7a35905f2cea1f6789494c3f971d711b05780e8
hjkl950217 <584880422@qq.com>
2020-11-23 21:22:24 +0800

    完成基本模式

--- CkTools/Src/CkTools.FP/CkFunctions/CkFunction_PipeIf.cs
+++ CkTools/Src/CkTools.FP/CkFunctions/CkFunction_PipeIf.cs
@@ -8,17 +8,42 @@ namespace CkTools.FP
     /// </summary>
     public static partial class CkFunctions
     {
-        #region Action - 0入参 0出参
-
-        #endregion Action - 0入参 0出参
-
         #region Action - 1入参 0出参
 
+        /// <summary>
+        /// 管道
+        /// </summary>
+        /// <Value>
+        /// <para><paramref name="exp"/>：将要执行的处理 </para>
+        /// <para><paramref name="judgeExp"/>：判断是否执行，返回true为执行 </para>
+        /// </Value>
+        /// <typeparam name="TInput">可传递任意类型</typeparam>
+        /// <returns></returns>
+        public static Action<TInput> PipeIf<TInput>(
+            [NotNull] Action<TInput> exp,
+            [NotNull] Func<TInput, bool> judgeExp)
+        {
+            return CkFunctions.If<TInput>(t => { }, exp, judgeExp);
+        }
+
+        /// <summary>
+        /// 管道
+        /// </summary>
+        /// <Value>
+        /// <para><paramref name="exp"/>：将要执行的处理 </para>
+        /// <para><paramref name="judgeExp"/>：判断是否执行，返回true为执行 </para>
+        /// </Value>
+        /// <typeparam name="TInput">可传递任意类型</typeparam>
+        /// <returns></returns>
+        public static Action<TInput> PipeIf<TInput>(
+            [NotNull] Action<TInput> exp,
+            [NotNull] Func<bool> judgeExp)
+        {
+            judgeExp.CheckNullWithException(nameof(judgeExp));
+            return CkFunctions.PipeIf<TInput>(exp, t => judgeExp());
+        }
+
         #endregion Action - 1入参 0出参
-
-        #region Func - 0入参 1出参
-
-        #endregion Func - 0入参 1出参
 
         #region Fun - 1入参 1出参
 
